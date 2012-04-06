65072e6ef681a4ea7529cf49aeac39fd6d1a64aa
Fajr Febriansyah <fajr.hf@gmail.com>
2012-04-06 21:08:37 +0700

    change content

--- alwayseveryday/application/modules/gallery/views/partials/header.php
+++
@@ -1,5 +0,0 @@
-		<div id="header" class="column span-24 transbox rounded last prepend-top">
-			<div id="navigation">
-				<a class="navlink" href="<?php echo base_url();?>">Home</a> | <a class="navlink" href="<?php echo base_url();?>index.php/profile">profile</a> | <a class="navlink" href="<?php echo base_url();?>index.php/news">news</a> | <a class="navlink" href="<?php echo base_url();?>index.php/page/pageindex/1">schedule</a> | <a class="navlink" href="<?php echo base_url();?>index.php/page/pageindex/2">contact</a> | <a class="navlink" href="#">pojok everyday</a>
-			</div>
-		</div>
--- alwayseveryday/assets/facebook/database.sql
+++
@@ -1,8 +0,0 @@
-
-create table comments
-(
-com_id int primary key auto_increment,
- comment varchar(200), 
-msg_id_fk int, 
-foreign key (msg_id_fk) references messages(msg_id)
-);
